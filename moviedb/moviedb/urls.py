ef3506c8a4cba82dafa7d1bbef97ac1b0d4c7fc0
Mateo mojica <mmojicah.84@gmail.com>
2025-10-03 01:56:53 +0000

    add django rest framework to the project

--- moviedb/moviedb/urls.py
+++ moviedb/moviedb/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('movies/', include('watchlist_app.urls')),
+    path('api-auth', include('rest_framework.urls')),
 ]
