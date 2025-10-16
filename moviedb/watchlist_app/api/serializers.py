7e7ad87e08920f68603112fa70c2ebb452f1ad8d
Mateo mojica <mmojicah.84@gmail.com>
2025-10-16 02:25:33 +0000

    Add three types of validations to the serializer

--- moviedb/watchlist_app/api/serializers.py
+++ moviedb/watchlist_app/api/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+# Validator
+def desc_length(value):
+    if len(value) < 2:
+            raise serializers.ValidationError('Description is too short')
+    else:
+        return value
+
 # Regular Serializer
 # https://www.django-rest-framework.org/api-guide/serializers/#saving-instances
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    # Validate using a validator
+    description = serializers.CharField(validators=[desc_length])
     active = serializers.BooleanField()
 
     def create(self, validated_data):
@@ -19,3 +27,18 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
 
         return instance
+    
+    # Field level validation
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Name is too short')
+        else:
+            return value
+        
+    # Object level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError(' Name and Description must be different')
+        else:
+            return data
+        
