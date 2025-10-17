b091ddb73111bab9fd70b56559f032169849c798
Mateo mojica <mmojicah.84@gmail.com>
2025-10-17 01:29:25 +0000

    Change the movie serializer to a model serializer

--- moviedb/watchlist_app/api/serializers.py
+++ moviedb/watchlist_app/api/serializers.py
@@ -1,33 +1,19 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-# Validator
-def desc_length(value):
-    if len(value) < 2:
-            raise serializers.ValidationError('Description is too short')
-    else:
-        return value
+# Model serializer
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
 
-# Regular Serializer
-# https://www.django-rest-framework.org/api-guide/serializers/#saving-instances
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only = True)
-    name = serializers.CharField()
-    # Validate using a validator
-    description = serializers.CharField(validators=[desc_length])
-    active = serializers.BooleanField()
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        # fields = ['id', 'description', 'name', 'len_name']
+        # exclude = ['active']
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
+    def get_len_name(self, object):
+        return len(object.name)
 
-        return instance
-    
     # Field level validation
     def validate_name(self, value):
         if len(value) < 2:
@@ -41,4 +27,4 @@ class MovieSerializer(serializers.Serializer):
             raise serializers.ValidationError(' Name and Description must be different')
         else:
             return data
-        
+
